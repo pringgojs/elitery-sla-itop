792c7d44ba81b3e5a5959bd9477581e3f61f3955
Pringgo Juni S <odyinggo@gmail.com>
2025-05-15 08:59:16 +0700

    remove lazy loading in filter component

--- resources/views/livewire/pages/dashboard/index.blade.php
+++ resources/views/livewire/pages/dashboard/index.blade.php
@@ -8,7 +8,7 @@
     </div>
 
     {{-- default filter: dateType = bulan ini, type: [UserRequest]  --}}
-    <livewire:utils.filter lazy table="pages.dashboard.section.data" date-type="this-month" require-type="true"
+    <livewire:utils.filter table="pages.dashboard.section.data" date-type="this-month" require-type="true"
         :type="['id' => 'UserRequest', 'name' => 'User Request']" use-type="true" use-status="false" use-date="true" use-search="false" use-download="true"
         use-organization="true" use-caller="false" use-team="false" use-agent="false" use-date-today="false"
         use-date-this-month="true" use-date-other-month="true" use-date-other-year="false" use-date-range="false" />
